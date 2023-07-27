4ac4082d1d708aa28ccb8f2bc38086e6e38d58fb
Nan Jiang <ibiza.jiang@outlook.com>
2023-07-27 21:43:31 +0800

    feat: 完善适配宏, 修改info.plist文件

--- NeteaseCloudMusic/Utils/NCScreen.h
+++ NeteaseCloudMusic/Utils/NCScreen.h
@@ -20,23 +20,33 @@ NS_ASSUME_NONNULL_BEGIN
 #define TOP_HEIGHT (STATUSBAR_HEIGHT + NAVIGATIONBAR_HEIGHT)
 
 // 底部高度
-#define SAFEDISTANCE_BOTTOM safeDistanceBottom()
+#define HOMEINDICATOR_HEIGHT homeIndicatorHeight()
 #define TABBAR_HEIGHT (49)
 // 下面的整体高度
-#define BOTTOM_HEIGHT (SAFEDISTANCE_BOTTOM + TABBAR_HEIGHT)
+#define BOTTOM_HEIGHT (HOMEINDICATOR_HEIGHT + TABBAR_HEIGHT)
 
 #define UI(x) UIAdapter(x)
 #define UIRect(x, y, width, height) UIRectAdapter(x, y, width, height)
+#define UISize(width, height) UISizeAdapter(width, height)
+#define UIPoint(x, y) UIPointAdapter(x, y)
 
 /// 根据iPhone 13/iPhone 13 Pro/iPhone 12/iPhone 12 Pro设置
 /// @param x 在iPhone 13/iPhone 13 Pro/iPhone 12/iPhone 12 Pro机型上布局的数值
 static inline NSInteger UIAdapter (float x) {
-    CGFloat scale = 390 / SCREEN_WIDTH;  // iPhone 13/iPhone 13 Pro/iPhone 12/iPhone 12 Pro 屏幕宽度
+    CGFloat scale = 390 / SCREEN_WIDTH;  // 390: iPhone 13/iPhone 13 Pro/iPhone 12/iPhone 12 Pro 屏幕宽度
     return (NSInteger)x / scale;
 }
 
 static inline CGRect UIRectAdapter(x, y, width, height) {
     return CGRectMake(UIAdapter(x), UIAdapter(y), UIAdapter(width), UIAdapter(height));
+}
+
+static inline CGSize UISizeAdapter(width, height) {
+    return CGSizeMake(UIAdapter(width), UIAdapter(height));
+}
+
+static inline CGPoint UIPointAdapter(x, y) {
+    return CGPointMake(UIAdapter(x), UIAdapter(y));
 }
 
 /// 顶部状态栏高度
@@ -47,8 +57,8 @@ static inline NSInteger statusBarHeight() {
     return statusBarManager.statusBarFrame.size.height;
 }
 
-/// 底部安全区高度
-static inline NSInteger safeDistanceBottom() {
+/// 底部home indicator高度
+static inline NSInteger homeIndicatorHeight() {
     NSSet *set = [[UIApplication sharedApplication] connectedScenes];
     UIWindowScene *windowScene = [set anyObject];
     UIWindow *window = windowScene.windows.firstObject;
