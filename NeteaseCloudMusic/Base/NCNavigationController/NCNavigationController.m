d721fa51ac0f863281c8db0bfe59ca6bdb1bfe51
Nan Jiang <ibiza.jiang@outlook.com>
2023-07-30 15:24:31 +0800

    feat: 制作navigationController的转场手势

--- NeteaseCloudMusic/Base/NCNavigationController/NCNavigationController.m
+++ NeteaseCloudMusic/Base/NCNavigationController/NCNavigationController.m
@@ -6,14 +6,15 @@
 //
 
 #import "NCNavigationController.h"
-#import "NCBaseViewController.h"
 #import "NCNavigationControllerPushAnimatedTransitionBottomToTop.h"
 #import "NCNavigationControllerPopAnimatedTransitionTopToBottom.h"
+#import "NCScreen.h"
+#import "NCBaseViewController.h"
+
 
 @interface NCNavigationController ()<UINavigationControllerDelegate, UIGestureRecognizerDelegate>
 
-@property (nonatomic, assign, readwrite) NCNavigationControllerPushOperations currentPushOperation;
-
+@property (nonatomic, strong, readwrite) UIPercentDrivenInteractiveTransition *popInteractiveTransition;
 @property (nonatomic, strong, readwrite) UIPanGestureRecognizer *panGestureRecognizer;
 // 从边缘滑动的手势
 @property (nonatomic, strong, readwrite) UIScreenEdgePanGestureRecognizer *screenEdgePanGestureRecognizer;
@@ -25,7 +26,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-//    self.navigationBar.translucent = NO;
+    self.navigationBar.translucent = NO;
     self.delegate = self;
     self.interactivePopGestureRecognizer.delegate = self;
 }
@@ -60,7 +61,44 @@
 #pragma mark - Private Method
 
 - (void)_handleGesture:(UIPanGestureRecognizer *)gesture {
-    NSLog(@"");
+    CGFloat process = 0.0;
+    CGFloat velocity = 0.0;
+    BOOL isScreenEdge = NO;
+    
+    // 测滑处理
+    if (self.screenEdgePanGestureRecognizer == gesture) {
+        velocity = [self.screenEdgePanGestureRecognizer velocityInView:self.screenEdgePanGestureRecognizer.view].x;
+        process = [self.screenEdgePanGestureRecognizer translationInView:self.screenEdgePanGestureRecognizer.view].x / SCREEN_WIDTH;
+        process = pow(MIN(1.0, MAX(0.0, process)), 2); // 增加手势梯度
+        isScreenEdge = YES;
+    } else {
+        velocity = [self.panGestureRecognizer velocityInView:self.panGestureRecognizer.view].y;
+        process = [self.panGestureRecognizer translationInView:self.panGestureRecognizer.view].y / (SCREEN_HEIGHT - UI(190));
+        process = pow(MIN(1.0, MAX(0.0, process)), 2); // 增加手势梯度
+    }
+    
+    if (gesture.state == UIGestureRecognizerStateBegan) {
+        self.popInteractiveTransition = [[UIPercentDrivenInteractiveTransition alloc] init];
+        [self popViewControllerAnimated:YES];
+    } else if (gesture.state == UIGestureRecognizerStateChanged) {
+        [self.popInteractiveTransition updateInteractiveTransition:process];
+    } else if (gesture.state == UIGestureRecognizerStateEnded || gesture.state == UIGestureRecognizerStateCancelled) {
+        if (isScreenEdge) {
+            if (velocity > 1000 || process > 0.2) {
+                [self.popInteractiveTransition finishInteractiveTransition];
+            } else {
+                [self.popInteractiveTransition cancelInteractiveTransition];
+            }
+        } else {
+            if (velocity > 1500 || process > 0.2) {
+                [self.popInteractiveTransition finishInteractiveTransition];
+            } else {
+                [self.popInteractiveTransition cancelInteractiveTransition];
+            }
+        }
+        self.popInteractiveTransition = nil;
+    }
+
 }
 
 #pragma mark - UINavigationControllerDelegate
@@ -79,7 +117,7 @@
     }
 }
 
-// 转场动画, 解决自定义的BottomUp的动画逻辑
+// 非交互手势转场
 - (nullable id <UIViewControllerAnimatedTransitioning>)navigationController:(UINavigationController *)navigationController
                                    animationControllerForOperation:(UINavigationControllerOperation)operation
                                                 fromViewController:(UIViewController *)fromVC
@@ -96,12 +134,39 @@
         }
     }
     return nil;
-
 }
 
+// 交互手势转场
 - (id<UIViewControllerInteractiveTransitioning>)navigationController:(UINavigationController *)navigationController interactionControllerForAnimationController:(id<UIViewControllerAnimatedTransitioning>)animationController {
-    NSLog(@"");
+    if ([animationController isKindOfClass:[NCNavigationControllerPopAnimatedTransitionTopToBottom class]]) {
+        return self.popInteractiveTransition;
+    }
     return nil;
+}
+
+#pragma mark - UIGestureRecognizerDelegate
+
+// 判断手势是否复合条件
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+    if (self.viewControllers.count <= 1) {
+        return NO;
+    }
+
+    // 处理符合边缘的手势
+    if (self.screenEdgePanGestureRecognizer == gestureRecognizer) {
+        CGPoint beginPoint = [gestureRecognizer locationInView:gestureRecognizer.view];
+        CGFloat maxAllowDistance = UI(20.0);
+        if (beginPoint.x > maxAllowDistance) {
+            return NO;
+        }
+    }
+
+    // 正在转场，不再重新开始一个新的转场
+    if ([self valueForKey:@"_isTransitioning"] != nil && [[self valueForKey:@"_isTransitioning"] boolValue] == YES) {
+        return NO;
+    }
+
+    return YES;
 }
 
 @end
