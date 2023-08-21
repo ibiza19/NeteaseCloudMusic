8856de80138ea32542ce23119ff62fa61451b153
Nan Jiang <ibiza.jiang@outlook.com>
2023-08-21 20:31:17 +0800

    feat: 实现上一曲/下一曲功能，为ListManager添加工具类，实现播放模式切换

---
+++ NeteaseCloudMusic/Manager/Utils/NCListManagerIndexUtils.m
@@ -0,0 +1,12 @@
+//
+//  NCListManagerIndexUtils.m
+//  NeteaseCloudMusic
+//
+//  Created by IBIZA on 8/21/23.
+//
+
+#import "NCListManagerIndexUtils.h"
+
+@implementation NCListManagerIndexUtils
+
+@end
---
+++ NeteaseCloudMusic/Utils/NCPlayModeTypeEnum.m
@@ -0,0 +1,12 @@
+//
+//  NCPlayModeTypeEnum.m
+//  NeteaseCloudMusic
+//
+//  Created by IBIZA on 8/21/23.
+//
+
+#import "NCPlayModeTypeEnum.h"
+
+@implementation NCPlayModeTypeEnum
+
+@end
